1cc3d42882f0d13f9f3bc28ba662b3dd23387f1b
weijunze <xaweijunze@163.com>
2023-09-04 16:42:18 +0800

    init-初始化路由初始化接口请求代理配置

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,14 +1,19 @@
 <template>
+  <view
+    >说明：本页面演示了仅依靠 history & location 对象实现页面路由功能，未使用
+    vue-router 的功能</view
+  >
 
-  <view>说明：本页面演示了仅依靠 history & location 对象实现页面路由功能，未使用 vue-router 的功能</view>
-
-  <view>页面href：{{currentHref}}</view>
+  <view>页面href：{{ currentHref }}</view>
 
   <nut-divider dashed>操作区</nut-divider>
   <nut-button type="default" @click="showHistory">history</nut-button>
   <nut-button type="default" @click="showLocation">location</nut-button>
   <nut-button type="default" @click="clickBack">back()</nut-button>
   <nut-button type="default" @click="clickForward">forward()</nut-button>
+  <nut-button type="default" @click="fetchApi"
+    >fetchApi()调用接口请求方法</nut-button
+  >
 
   <nut-divider dashed>点击Tab</nut-divider>
 
@@ -20,67 +25,79 @@
   <component :is="currentView" />
 
   <nut-divider dashed>跳转</nut-divider>
-  <nut-button block type="default" @click="goToPage('/pages/vue-router/index')">前往"vue-router"页面</nut-button>
-
+  <nut-button block type="default" @click="goToPage('/pages/vue-router/index')"
+    >前往"vue-router"页面</nut-button
+  >
 </template>
 
 <script>
 // 示例来源于Vue官方文档： https://staging-cn.vuejs.org/guide/scaling-up/routing.html#simple-routing-from-scratch
-import './index.scss'
-import Taro from '@tarojs/taro'
+import "./index.scss";
+import Taro from "@tarojs/taro";
 
-import Tab1 from './Tab1.vue'
-import Tab2 from './Tab2.vue'
-import Tab3 from './Tab3.vue'
+import api from "../../services/api";
+
+import Tab1 from "./Tab1.vue";
+import Tab2 from "./Tab2.vue";
+import Tab3 from "./Tab3.vue";
 const routes = {
-  '/': Tab1,
-  '/2': Tab2,
-  '/3': Tab3
-}
+  "/": Tab1,
+  "/2": Tab2,
+  "/3": Tab3,
+};
 
 export default {
   data() {
     return {
       currentPath: window.location.hash,
-      currentHref: window.location.href
-    }
+      currentHref: window.location.href,
+    };
   },
   methods: {
-    clickLink(event){
-      const hash = event.target.dataset.hash
-      window.history.pushState(null, '', hash)
+    clickLink(event) {
+      const hash = event.target.dataset.hash;
+      window.history.pushState(null, "", hash);
     },
 
-    clickBack(){
-      window.history.back()
+    clickBack() {
+      window.history.back();
     },
 
-    clickForward(){
-      window.history.forward()
+    clickForward() {
+      window.history.forward();
     },
 
-    showHistory(){
-      console.log('history', window.history)
+    showHistory() {
+      console.log("history", window.history);
     },
 
-    showLocation(){
-      console.log('location', window.location)
+    showLocation() {
+      console.log("location", window.location);
     },
-
-    goToPage(url){
-      Taro.navigateTo({url})
-    }
+    fetchApi() {
+      api
+        .get("/playlist/detail")
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    goToPage(url) {
+      Taro.navigateTo({ url });
+    },
   },
   computed: {
     currentView() {
-      return routes[this.currentPath.slice(1) || '/']
-    }
+      return routes[this.currentPath.slice(1) || "/"];
+    },
   },
   mounted() {
-    window.addEventListener('hashchange', () => {
-		  this.currentPath = window.location.hash
-      this.currentHref = window.location.href
-		})
-  }
-}
+    window.addEventListener("hashchange", () => {
+      this.currentPath = window.location.hash;
+      this.currentHref = window.location.href;
+    });
+  },
+};
 </script>
